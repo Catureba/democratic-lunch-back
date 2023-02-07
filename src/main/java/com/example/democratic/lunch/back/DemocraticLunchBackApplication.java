bb6bc05877f82a69975d2e2be6ec2fddef8c39d8
catureba <caturebadev@gmail.com>
2023-02-07 10:23:56 -0300

    Docker and Postgres configurations

---
+++ src/main/java/com/example/democratic/lunch/back/DemocraticLunchBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.democratic.lunch.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocraticLunchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocraticLunchBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/democratic/lunch/back/DemocraticLunchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democratic.lunch.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocraticLunchBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
