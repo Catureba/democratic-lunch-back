0fb9aa4819ec0d920a9d530a8ce9fed3fac5294d
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-02-08 13:39:10 -0300

    Login de usuário

--- src/main/java/com/example/democratic/lunch/back/domain/model/User.java
+++ src/main/java/com/example/democratic/lunch/back/domain/model/User.java
@@ -3,6 +3,12 @@ package com.example.democratic.lunch.back.domain.model;
 import com.example.democratic.lunch.back.domain.dto.RegisterUserData;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "users")
 @Entity(name = "User")
@@ -11,7 +17,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,5 +29,35 @@ public class User {
         this.name = data.name();
         this.email = data.email();
         this.password = data.password();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
